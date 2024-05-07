14b66fda3030ccb5e7a67672da448e122248acd0
Jan Kuri <jkuri88@gmail.com>
2024-05-07 08:17:42 +0200

    Update rollup config and tests

--- src/__tests__/slider.js
+++ src/__tests__/slider.js
@@ -1,84 +1,99 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
 import Slider from '../slider';
+import '@testing-library/jest-dom';
 
-test('x', () => {
-  const component = renderer.create(<Slider />);
-  expect(component.toJSON()).toMatchInlineSnapshot(`
-    <div
-      className="css-mcm35l"
-      onMouseDown={[Function]}
-      onTouchStart={[Function]}
-    >
+describe('Slider Component', () => {
+  test('renders correctly with default props', () => {
+    const { container } = render(<Slider />);
+    const slider = container.firstChild;
+
+    expect(slider).toHaveClass('css-gvm8wn-Slider');
+    expect(slider).toMatchSnapshot(`
       <div
-        className="css-c5m0sj"
-        style={
-          Object {
-            "width": "50%",
-          }
-        }
-      />
-      <div
-        onClick={[Function]}
+        className="css-mcm35l"
         onMouseDown={[Function]}
         onTouchStart={[Function]}
-        style={
-          Object {
-            "left": "50%",
-            "position": "absolute",
-            "top": "50%",
-            "transform": "translate(-50%, -50%)",
-          }
-        }
       >
         <div
-          className="css-p9m01q"
-        />
-      </div>
-    </div>
-  `);
-});
-
-test('y', () => {
-  const component = renderer.create(<Slider axis="y" />);
-  expect(component.toJSON()).toMatchInlineSnapshot(`
-    <div
-      className="css-1munbi2"
-      onMouseDown={[Function]}
-      onTouchStart={[Function]}
-    >
-      <div
-        className="css-ee7l6q"
-        style={
-          Object {
-            "height": "50%",
+          className="css-c5m0sj"
+          style={
+            Object {
+              "width": "50%",
+            }
           }
-        }
-      />
+        />
+        <div
+          onClick={[Function]}
+          onMouseDown={[Function]}
+          onTouchStart={[Function]}
+          style={
+            Object {
+              "left": "50%",
+              "position": "absolute",
+              "top": "50%",
+              "transform": "translate(-50%, -50%)",
+            }
+          }
+        >
+          <div
+            className="css-p9m01q"
+          />
+        </div>
+      </div>
+  `);
+  });
+
+  test('renders correctly with axis="y"', () => {
+    const { container } = render(<Slider axis="y" />);
+    const slider = container.firstChild;
+
+    expect(slider).toHaveClass('css-11fxyc7-Slider');
+    expect(slider).toMatchSnapshot(`
       <div
-        onClick={[Function]}
+        className="css-1munbi2"
         onMouseDown={[Function]}
         onTouchStart={[Function]}
-        style={
-          Object {
-            "left": "50%",
-            "position": "absolute",
-            "top": "50%",
-            "transform": "translate(-50%, -50%)",
-          }
-        }
       >
         <div
-          className="css-p9m01q"
+          className="css-ee7l6q"
+          style={
+            Object {
+              "height": "50%",
+            }
+          }
         />
+        <div
+          onClick={[Function]}
+          onMouseDown={[Function]}
+          onTouchStart={[Function]}
+          style={
+            Object {
+              "left": "50%",
+              "position": "absolute",
+              "top": "50%",
+              "transform": "translate(-50%, -50%)",
+            }
+          }
+        >
+          <div
+            className="css-p9m01q"
+          />
+        </div>
       </div>
-    </div>
   `);
-});
+  });
 
-test('xy', () => {
-  const component = renderer.create(<Slider axis="xy" />);
-  expect(component.toJSON()).toMatchInlineSnapshot(`
+  test('renders correctly with axis="xy"', () => {
+    const { container } = render(<Slider axis="xy" />);
+    const slider = container.firstChild;
+
+    expect(slider).toHaveClass('css-1ed3i9o-Slider');
+    expect(slider).toMatchSnapshot(`
     <div
       className="css-1rhaxo2"
       onMouseDown={[Function]}
@@ -107,4 +122,5 @@ test('xy', () => {
       </div>
     </div>
   `);
+  });
 });
--- src/slider.js
+++ src/slider.js
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import { jsx } from '@emotion/core';
+import { jsx } from '@emotion/react';
 import { useRef } from 'react';
 import { getClientPosition } from './utils';
 import defaultStyles from './styles';
